912801d168e425fef107de3fbcd0a9d4b622d7a5
Nwovu Sunday <honesty1ofa1i@gmail.com>
2021-07-23 00:17:29 +0100

    Updated secret key

--- hcf_library/settings.py
+++ hcf_library/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-!cs7sm9j=ih$l&&y0)jy98w-e4k677+_5w383i20py8y_2tlf4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
